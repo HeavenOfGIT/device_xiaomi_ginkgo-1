d7d1811864f4b416c1a5d86bd8373452586086d3
xawlw <abdulazizawlw@gmail.com>
2020-10-13 01:28:08 +0530

    Xtended-ify
    Signed-off-by: xawlw <abdulazizawlw@gmail.com>

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@ $(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
 # Gapps
 $(call inherit-product-if-exists, vendor/google/gms/config.mk)
 
-# Pixel components
-$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
--- xtended_ginkgo.mk
+++ xtended_ginkgo.mk
@@ -23,8 +23,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
 # Device identifier
-PRODUCT_NAME := aosp_ginkgo
+PRODUCT_NAME := xtended_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
@@ -39,3 +42,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
+
+# Xtended stuff
+XTENDED_MAINTAINER := xawlw
+XTENDED_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
